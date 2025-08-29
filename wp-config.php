35fe905222826ab91bc47825ba626f8d37e5bf92
Cristian Mauricio Barajas Carreño <cbarajas@intelecto.co>
2025-08-28 22:30:23 -0500

    Se despliega CRUD de canales de comunicación y pluggin de logos de login y admin tool bar

--- wp-config.php
+++ wp-config.php
@@ -2,11 +2,12 @@
 /** Enable W3 Total Cache */
 define('WP_CACHE', true); // Added by W3 Total Cache
 
+
 /**
  * The base configuration for WordPress
  *
  * The wp-config.php creation script uses this file during the installation.
- * You don't have to use the website, you can copy this file to "wp-config.php"
+ * You don't have to use the web site, you can copy this file to "wp-config.php"
  * and fill in the values.
  *
  * This file contains the following configurations:
@@ -16,79 +17,96 @@ define('WP_CACHE', true); // Added by W3 Total Cache
  * * Database table prefix
  * * ABSPATH
  *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/
+ * @link https://wordpress.org/support/article/editing-wp-config-php/
  *
  * @package WordPress
  */
 
+//Using environment variables for memory limits
+$wp_memory_limit = (getenv('WP_MEMORY_LIMIT') && preg_match("/^[0-9]+M$/", getenv('WP_MEMORY_LIMIT'))) ? getenv('WP_MEMORY_LIMIT') : '128M';
+$wp_max_memory_limit = (getenv('WP_MAX_MEMORY_LIMIT') && preg_match("/^[0-9]+M$/", getenv('WP_MAX_MEMORY_LIMIT'))) ? getenv('WP_MAX_MEMORY_LIMIT') : '256M';
+
+/** General WordPress memory limit for PHP scripts*/
+define('WP_MEMORY_LIMIT', $wp_memory_limit );
+
+/** WordPress memory limit for Admin panel scripts */
+define('WP_MAX_MEMORY_LIMIT', $wp_max_memory_limit );
+
+
+//Using environment variables for DB connection information
+
 // ** Database settings - You can get this info from your web host ** //
+$connectstr_dbhost = getenv('DATABASE_HOST');
+$connectstr_dbname = getenv('DATABASE_NAME');
+$connectstr_dbusername = getenv('DATABASE_USERNAME');
+$connectstr_dbpassword = getenv('DATABASE_PASSWORD');
+
+// Using managed identity to fetch MySQL access token
+if (strtolower(getenv('ENABLE_MYSQL_MANAGED_IDENTITY')) === 'true') {
+	try {
+		require_once(ABSPATH . 'class_entra_database_token_utility.php');
+		if (strtolower(getenv('CACHE_MYSQL_ACCESS_TOKEN')) !== 'true') {
+			$connectstr_dbpassword = EntraID_Database_Token_Utilities::getAccessToken();
+		} else {
+			$connectstr_dbpassword = EntraID_Database_Token_Utilities::getOrUpdateAccessTokenFromCache();
+		}
+	} catch (Exception $e) {
+		// An empty string displays a 502 HTTP error page rather than a database connection error page. So, using a dummy string instead.
+		$connectstr_dbpassword = '<dummy-value>';
+		error_log($e->getMessage());
+	}
+}
+
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'hifix' );
+define('DB_NAME', $connectstr_dbname);
 
-/** Database username */
-define( 'DB_USER', 'dbadmin' );
+/** MySQL database username */
+define('DB_USER', $connectstr_dbusername);
 
-/** Database password */
-define( 'DB_PASSWORD', 'tP573u%u-0q2' );
+/** MySQL database password */
+define('DB_PASSWORD',$connectstr_dbpassword);
 
-/** Database hostname */
-define( 'DB_HOST', 'mssql-weastus2-hifix-prd.mysql.database.azure.com:3306' );
+/** MySQL hostname */
+define('DB_HOST', $connectstr_dbhost);
 
-/** Database charset to use in creating database tables. */
-define( 'DB_CHARSET', 'utf8mb3' );
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
 
-/** The database collate type. Don't change this if in doubt. */
+/** The Database Collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
 
-// ================================================
-// 🔐 Configuración SSL para MySQL en Azure
-// ================================================
-define( 'MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL );
-
-// Ruta al certificado raíz de Azure
-// 👉 Descarga desde: https://dl.cacerts.digicert.com/BaltimoreCyberTrustRoot.crt.pem
-// y guárdalo en /certs dentro de tu proyecto
-define( 'DB_SSL_CA', __DIR__ . '/certs/BaltimoreCyberTrustRoot.crt.pem' );
-
-// Forzar que WP use SSL en la conexión MySQL
-if ( ! defined( 'PDO::MYSQL_ATTR_SSL_CA' ) ) {
-    define( 'PDO::MYSQL_ATTR_SSL_CA', DB_SSL_CA );
+/** Enabling support for connecting external MYSQL over SSL*/
+$mysql_sslconnect = (getenv('DB_SSL_CONNECTION')) ? getenv('DB_SSL_CONNECTION') : 'true';
+if (strtolower($mysql_sslconnect) != 'false' && !is_numeric(strpos($connectstr_dbhost, "127.0.0.1")) && !is_numeric(strpos(strtolower($connectstr_dbhost), "localhost"))) {
+	define('MYSQL_CLIENT_FLAGS', MYSQLI_CLIENT_SSL);
 }
 
 
 /**#@+
- * Authentication unique keys and salts.
+ * Authentication Unique Keys and Salts.
  *
- * Change these to different unique phrases! You can generate these using
- * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
- *
- * You can change these at any point in time to invalidate all existing cookies.
- * This will force all users to have to log in again.
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         'iK%wR!4C]O7C*D]^:DR9~R&A@-;sjK-]{vH{$aOUt>@pn^[=vHJq#`h]/s&SPqJ/' );
-define( 'SECURE_AUTH_KEY',  'l>x3>dSeB%+Zqvt+r{b_p~#v/{EICN7Z6<_IE&HDH`4Cb:8+[[i)3yKHw;WkeH*V' );
-define( 'LOGGED_IN_KEY',    ')3)LjtKzd,6fkIXk7zL=5i72+Q=w)IGuh7F_s;xO+Ht,c_6mWvCt/yE@*3dBL-~t' );
-define( 'NONCE_KEY',        ')k*7vs@2}T> >dA$!HH7?7m77]_=6PqL3iAU:uVT!RX=bxu6E$D)4)Ry3=BP[,YC' );
-define( 'AUTH_SALT',        '~rjjLU!6,okT)O:0-Zk3<t_9v9#=KycxQ~y%UOWjuM|8IJc.NPA$<,CS9D ^J37z' );
-define( 'SECURE_AUTH_SALT', ')}Hek+:|Ve)(dU2P=mfs?s#dYIL`/[^:T;Kgi^Xxq*`Cd%D|q0*7:&hXCPihd7rB' );
-define( 'LOGGED_IN_SALT',   'TqDHG~E|BY--a#oMw2dk:?D9ur[A!tuwP/rry>m9Y>y@kWRgXO[!v`4kh!|lfUiG' );
-define( 'NONCE_SALT',       ':S3Pb0X^Frjh~9$>UG1Xuw9B;Ju^Ptk$sfV.u2KDzG4NTO?7UZ^C!U_p !$qsNWg' );
+define( 'AUTH_KEY',         'tWKI=aO_b#^ct[#r$Fet`OuBF$#Fcv@8uIh<m5|yQq/zAE-K_q~^@V}0c@1$Y|i:' );
+define( 'SECURE_AUTH_KEY',  'Zk%aZe18>2cce67)hYim,MaoQVq$kA}P*+ 5ZFa4k2n#Ak;W<Zn `0j@: V%c.N>' );
+define( 'LOGGED_IN_KEY',    '}EN&6 Yr@PL!NjpbToR|};I:p,Wi9xjh~{mEoB47$[A#F.AJFOYe:IN^h^_nL^YM' );
+define( 'NONCE_KEY',        '`A{a^lY7B^HG-IeL5g4w(o44x!|i[|sG5gfs/<r$b/V 0[MK/)nTTN}C*+<<ziSs' );
+define( 'AUTH_SALT',        'LI1%Q#Jk3TMM8n=J<Kt]:a4<Gh|YYL&!<2N}im#9$R&14+?/C%_212)<B3zg-mzA' );
+define( 'SECURE_AUTH_SALT', ';DvKI~zwyf3!c2y()tj+E.zXK17izHRnmje=L#:5;y9@eJ{VW5U N_Itq-Rz4zyS' );
+define( 'LOGGED_IN_SALT',   'W#j^F1PiTA4q3N,]DJ p}GG*cUldz(HGCt,vqflrHf?ld1Qnn%Mi1neF _Sr>gK~' );
+define( 'NONCE_SALT',       '*|5vp^?wh>0$r4UQZ1$4r`%Sshtd|}r ^kC*;HsS=3L`M+L|AXQ.(L}<H6b;n!a[' );
 
 /**#@-*/
 
 /**
- * WordPress database table prefix.
+ * WordPress Database Table prefix.
  *
  * You can have multiple installations in one database if you give each
  * a unique prefix. Only numbers, letters, and underscores please!
- *
- * At the installation time, database tables are created with the specified prefix.
- * Changing this value after WordPress is installed will make your site think
- * it has not been installed.
- *
- * @link https://developer.wordpress.org/advanced-administration/wordpress/wp-config/#table-prefix
  */
 $table_prefix = 'wp_';
 
@@ -102,17 +120,25 @@ $table_prefix = 'wp_';
  * For information on other constants that can be used for debugging,
  * visit the documentation.
  *
- * @link https://developer.wordpress.org/advanced-administration/debug/debug-wordpress/
+ * @link https://wordpress.org/support/article/debugging-in-wordpress/
  */
 define( 'WP_DEBUG', false );
 
-/* Add any custom values between this line and the "stop editing" line. */
+/* That's all, stop editing! Happy blogging. */
+/**https://developer.wordpress.org/reference/functions/is_ssl/ */
+if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https')
+	$_SERVER['HTTPS'] = 'on';
 
-// Forzar WordPress a usar localhost
-define('WP_HOME','http://localhost/wordpress');
-define('WP_SITEURL','http://localhost/wordpress');
+$http_protocol='http://';
+if (!preg_match("/^localhost(:[0-9])*/", $_SERVER['HTTP_HOST']) && !preg_match("/^127\.0\.0\.1(:[0-9])*/", $_SERVER['HTTP_HOST'])) {
+	$http_protocol='https://';
+}
 
-/* That's all, stop editing! Happy publishing. */
+//Relative URLs for swapping across app service deployment slots
+define('WP_HOME', $http_protocol . $_SERVER['HTTP_HOST']);
+define('WP_SITEURL', $http_protocol . $_SERVER['HTTP_HOST']);
+define('WP_CONTENT_URL', '/wp-content');
+define('DOMAIN_CURRENT_SITE', $_SERVER['HTTP_HOST']);
 
 /** Absolute path to the WordPress directory. */
 if ( ! defined( 'ABSPATH' ) ) {
@@ -121,12 +147,3 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 /** Sets up WordPress vars and included files. */
 require_once ABSPATH . 'wp-settings.php';
-
-// ================================================
-// ⚡ Hook para aplicar SSL al handler mysqli
-// ================================================
-add_action( 'wpdb_init', function( $wpdb ) {
-    if ( defined('DB_SSL_CA') && file_exists(DB_SSL_CA) ) {
-        mysqli_ssl_set( $wpdb->dbh, NULL, NULL, DB_SSL_CA, NULL, NULL );
-    }
-});
